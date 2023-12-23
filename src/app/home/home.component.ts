6983f1bec7a99f1de15774c127685e028d777bbb
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-12-23 23:12:08 +0900

    feat: add if

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component } from "@angular/core";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { HousingLocation } from "../housinglocation";
 
 @Component({
   selector: "app-home",
   standalone: true,
+  imports: [HousingLocationComponent],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
+        <input type="text" placeholder="Filter by city" />
         <button type="button">Search</button>
       </form>
     </section>
+    <section>
+      <app-housing-location></app-housing-location>
+    </section>
   `,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = "https://angular.io/assets/images/tutorials/faa";
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: "Test Home",
+    city: "Test city",
+    state: "ST",
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-housing-location",
+  standalone: true,
+  templateUrl: "./housing-location.component.html",
+})
+export class HousingLocationComponent {}
