141a294a60ef031c81ce994a965e7588690c9edc
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-12-25 21:44:54 +0900

    feat: search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,15 @@ import { CommonModule } from "@angular/common";
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button type="button" (click)="filterResults(filter.value)">
+          Search
+        </button>
       </form>
     </section>
     <section>
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
       >
       </app-housing-location>
@@ -26,9 +28,23 @@ import { CommonModule } from "@angular/common";
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
   housingService = inject(HousingService);
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
