80133278b506082735bdee2114a6841c5256b8b5
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-12-25 20:38:30 +0900

    feat: test

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { HousingService } from "../housing.service";
 import { HousingLocation } from "../housinglocation";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-details",
   standalone: true,
+  imports: [ReactiveFormsModule],
   template: `
     <article>
       <img [src]="housingLocation?.photo"
@@ -22,16 +24,45 @@ import { HousingLocation } from "../housinglocation";
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
+      <section>
+        <h2>Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+
+          <button type="submit">Apply now</button>
+        </form>
+      </section>
     </article>
-    `
+  `,
 })
 export class DetailsComponent {
   route = inject(ActivatedRoute)
   housingService = inject(HousingService)
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+  }
 }
+
