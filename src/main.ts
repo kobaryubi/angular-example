b47d64e02510a9c53fc66d27049c281025320d86
Masahiko Kobayashi <kobaryubi6@gmail.com>
2023-12-30 16:33:24 +0900

    feat: app config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
-import routeConfig from "./app/routes";
+import { appConfig } from "./app/app.config";
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeConfig)],
-}).catch((err) => {
+bootstrapApplication(AppComponent, appConfig).catch((err) => {
   console.log(err);
 });
