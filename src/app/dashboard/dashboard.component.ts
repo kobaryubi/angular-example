ef1a27e85c0e1945f5708fbb7aece4cb77c72a88
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-01-01 16:55:39 +0900

    feat: detail

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,16 @@
+import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
-import { CommonModule } from "@angular/common";
-import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 import { HeroService } from "../hero.service";
 import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: "app-heroes",
+  selector: "app-dashboard",
   standalone: true,
-  imports: [CommonModule, RouterModule, HeroDetailComponent],
-  templateUrl: "./heroes.component.html",
+  imports: [CommonModule, RouterModule],
+  templateUrl: "./dashboard.component.html",
 })
-export class HeroesComponent implements OnInit {
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) {}
@@ -22,7 +21,7 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => {
-      this.heroes = heroes;
+      this.heroes = heroes.slice(1, 5);
     });
   }
 }
