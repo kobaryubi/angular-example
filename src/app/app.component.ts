03b07b61de71c7016ece694f44a298c361cf7170
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-01-01 14:26:56 +0900

    feat: outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { MessagesComponent } from "./messages/messages.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterModule],
-  template: `
-    <main>
-      <a [routerLink]="['/']">HOME</a>
-      <router-outlet></router-outlet>
-    </main>
-  `,
+  imports: [RouterModule, MessagesComponent],
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+  title = "Tour of Heroes";
+}
