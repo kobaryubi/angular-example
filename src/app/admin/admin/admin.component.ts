986d773dcdc999ef5196abf8331ac80863e9edbf
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-01-07 13:05:30 +0900

    feat: admin

---
+++ src/app/admin/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector: "app-admin",
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: "./admin.component.html",
+})
+export class AdminComponent {}
