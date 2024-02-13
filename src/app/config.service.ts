2b8b053e4707799f35e27022186a62c49a357fd1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-02-13 19:45:34 +0900

    feat: config

---
+++ src/app/config.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
+  date: any;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class ConfigService {
+  configUrl = "assets/config.json";
+
+  constructor(private http: HttpClient) {}
+
+  getConfig() {
+    return this.http.get<Config>(this.configUrl);
+  }
+}
---
+++ src/app/http-client/http-client.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+import { Config, ConfigService } from "../config.service";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-http-client",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./http-client.component.html",
+})
+export class HttpClientComponent {
+  config: Config | undefined;
+
+  constructor(private configService: ConfigService) {}
+
+  showConfig() {
+    this.configService.getConfig().subscribe((data) => {
+      this.config = {
+        heroesUrl: data.heroesUrl,
+        textfile: data.textfile,
+        date: data.date,
+      };
+    });
+  }
+}
